251926534e52b9a59008372e5f0a3fd641cbe6b1
ArhamazizNoman <arham.noman.50@gmail.com>
2026-01-17 11:55:22 +0100

    CI: add requirements.txt files, fix linting workflow, remove unused imports

--- tests/test_data_preprocess.py
+++ tests/test_data_preprocess.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 
 from project.data import preprocess, FinancialPhraseBankDataset
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils.data import DataLoader
 
 from project.data import FinancialPhraseBankDataset
