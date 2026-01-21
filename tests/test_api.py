fec0440e7a08b3a87ab2a17b53381814491c9bcf
Nikos Iliakis <nickiliak847@gmail.com>
2026-01-21 12:36:14 +0000

    Added ruff format pre commit

---
+++ tests/test_api.py
@@ -0,0 +1,13 @@
+
+import pytest
+from fastapi.testclient import TestClient
+from src.project.backend import app
+
+client = TestClient(app)
+
+def test_root_health_check():
+	response = client.get("/")
+	assert response.status_code == 200
+	data = response.json()
+	assert data["message"] == "OK"
+	assert data["status-code"] == 200
